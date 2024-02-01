0ad0c38dda7efdaca70f7a2ebfba8f88f6d9699b
PatrykSwoboda <125033099+PatrykSwoboda@users.noreply.github.com>
2024-02-01 17:51:22 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,16 @@ import streamlit as st
 
 st.title('Projekt zaliczeniowy z programowania dla analityki danych.')
 
-st.markdown("## Czyszczenie danych")
-
 data = st.file_uploader('Tu wrzuc swoje dane', type=['csv'])
 if data is not None:
     df = pd.read_csv(data, na_values=[" ", np.nan])
     st.dataframe(df.head(15))
+
+st.markdown("## Czyszczenie danych")
+
+page = st.sidebar.selectbox('Wybierz stronę:', [''])
+
+
 
 st.code("messy_data['Dimension_x'] = messy_data['Dimension_x'].replace(np.nan, np.random.normal(5.88, 0.77)).round(2)")
 
