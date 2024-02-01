3084aa13214f6f2d78b8c3053a8e1eff48b2ef49
PatrykSwoboda <125033099+PatrykSwoboda@users.noreply.github.com>
2024-02-01 17:39:40 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,11 @@ if data is not None:
 
 st.code("messy_data['Dimension_x'] = messy_data['Dimension_x'].replace(np.nan, np.random.normal(5.88, 0.77)).round(2)")
 
+data = st.file_uploader('Tu wrzuc swoje dane', type=['csv'])
+if data is not None:
+    df = pd.read_csv(data, na_values=[" ", np.nan])
+    st.dataframe(df.head(15))
+    
 px.box(messy_data['Price'], title="Boxplot of prices of diamonds with outliers", height=500)
 
 
