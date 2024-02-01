e8afd9036562800bde0f49e297b2c13b0060bbe1
PatrykSwoboda <125033099+PatrykSwoboda@users.noreply.github.com>
2024-02-01 17:08:08 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import numpy as np
-import streamlit as st
-import datetime
-import time
+import seaborn as sns
+import random
+import matplotlib.pyplot as plt
+import plotly.express as px
+import statsmodels.formula.api as smf
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 data = st.file_uploader('Tu wrzuc swoje dane', type=['csv'])
 if data is not None:
-    df = pd.read_csv(data)
+    df = pd.read_csv(data, na_values=[" ", np.nan])
     st.dataframe(df.head(10))
