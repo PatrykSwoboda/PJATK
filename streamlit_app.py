1e118f82647f243bec93edd8b8e9b4001911b06a
PatrykSwoboda <125033099+PatrykSwoboda@users.noreply.github.com>
2024-02-01 17:37:37 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,16 @@ import streamlit as st
 
 st.title('Projekt zaliczeniowy z programowania dla analityki danych.')
 
-st.markdown("# Czyszczenie danych")
+st.markdown("## Czyszczenie danych")
 
 data = st.file_uploader('Tu wrzuc swoje dane', type=['csv'])
 if data is not None:
     df = pd.read_csv(data, na_values=[" ", np.nan])
-    st.dataframe(df.head(10))
+    st.dataframe(df.head(15))
 
 st.code("messy_data['Dimension_x'] = messy_data['Dimension_x'].replace(np.nan, np.random.normal(5.88, 0.77)).round(2)")
+
+px.box(messy_data['Price'], title="Boxplot of prices of diamonds with outliers", height=500)
 
 
 
