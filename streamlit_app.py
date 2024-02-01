288a31f77c245560d126d291b45098d02347614a
PatrykSwoboda <125033099+PatrykSwoboda@users.noreply.github.com>
2024-02-01 15:00:20 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import datetime
+import time
 
+data = st.file_uploader('Tu wrzuc swoje dane', type=['csv'])
+if data is not None:
+    df.pd.read_csv(data)
+    st.dataframe(df.head(10))
