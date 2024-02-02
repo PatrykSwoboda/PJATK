073575025dc847ba104e0568b8e7dd1fd66e31c7
PatrykSwoboda <125033099+PatrykSwoboda@users.noreply.github.com>
2024-02-02 08:52:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,23 @@ import streamlit as st
 import plotly.express as px
 
 st.title('Projekt zaliczeniowy z programowania dla analityki danych.')
+page = st.sidebar.selectbox('Wybierz stronę:', [''])
 
 data = st.file_uploader('Tu wrzuc swoje dane', type=['csv'])
 if data is not None:
     df = pd.read_csv(data, na_values=[" ", np.nan])
     st.dataframe(df.head(15))
+    
     st.markdown("## Czyszczenie danych")
-    page = st.sidebar.selectbox('Wybierz stronę:', [''])
+    
     st.code("messy_data['Dimension_x'] = messy_data['Dimension_x'].replace(np.nan, np.random.normal(5.88, 0.77)).round(2)")
-    '''data2 = st.file_uploader('Tu wrzuc swoje dane', type=['csv'])
-    if data2 is not None:
-        df_2 = pd.read_csv(data2, na_values=[" ", np.nan])
-        st.dataframe(df_2.head(15))'''
-        
+
+    st.markdown("## Wizualizacja rozrzutów i rozkładów zmiennych")
+    df['Price'].describe()
+    df['Price'].nlargest(10)
+    
     col1, col2=st.columns(2)
-    fig=px.box(df['Price'], title="Boxplot of prices of diamonds with outliers", height=500)
+    fig=px.box(df['Price'], title="Boxplot of prices of diamonds without outliers", height=500)
     col1.plotly_chart(fig)
 
     col1, col2=st.columns(2)
